62173fd0995f9d20b8b16ad86db479e652ff083d
Metellus509 <metellusjunior56@gmail.com>
2025-11-26 20:19:29 +0000

    complete

--- INTERFACE/APP_MANAGER_FRONT_END/src/Pages/Dashboard.jsx
+++ INTERFACE/APP_MANAGER_FRONT_END/src/Pages/Dashboard.jsx
@@ -1,8 +1,10 @@
+import { useState,useEffect } from 'react';
 import Card from '../Components/Card.jsx'
 import SearchBar from '../Components/SearchBar.jsx';
 
 
 function Dashboard() {
+
 
   return (
     <>
@@ -24,7 +26,14 @@ function Dashboard() {
           </ul>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="#">Login</a>
+            <button type="button" class="btn btn-success btn-sm">
+            <span className="glyphicon glyphicon-log-out"></span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+              </svg>
+               LOGOUT
+            </button>
             </li>
           </ul>
         </div>
@@ -33,7 +42,7 @@ function Dashboard() {
   
     <SearchBar/>
 
-    <div className="">
+    <div className="container mx-auto">
       <nav aria-label="Page navigation example">
         <ul className="pagination">
           <li className="page-item">
@@ -51,8 +60,8 @@ function Dashboard() {
           </li>
         </ul>
       </nav>
-
     </div>
+
     <Card/>
     </>
   )
